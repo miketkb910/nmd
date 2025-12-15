02baf7a2d577d2911e4f5635c33afc546d7a6587
miketkb910 <bhesaniyamiket@gmail.com>
2025-12-15 10:21:36 +0530

    for coolify changes have made

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const mongoURI = "mongodb://abcuser:abcpass@mongo:27017/abcdb?authSource=admin";
 const connectToMongo = async () => {
   try {
     mongoose.set("strictQuery", false);
-    mongoose.connect(process.env.mongoURI);
+    mongoose.connect("mongodb://root:ZWDYpuem7HoBuHRxcUTGRNuVLL5sgaz3fm2SqZrCQfRkPGHGHSuhZDb2ONVNo6nC@z8cck48g4ws0k0ccgkc40kok:27017/?directConnection=true");
     console.log("Connected to Mongo Successfully!");
   } catch (error) {
     console.log(error);
