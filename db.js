c9c22a6993c006425a66ee8fc840065b1e4522dc
miketkb910 <bhesaniyamiket@gmail.com>
2025-12-15 10:59:35 +0530

    for coolify changes have made

--- db.js
+++ db.js
@@ -3,11 +3,12 @@ const mongoURI = "mongodb://abcuser:abcpass@mongo:27017/abcdb?authSource=admin";
 
 // if node and mongo deployed on container manually then mongoURI = "mongodb://abcuser:abcpass@mongo:27017/abcdb?authSource=admin" and mongoose.connect(mongoURI) like that
 // process.env.mongoURI if we use coolify to deploy both 
+// "mongodb://root:ZWDYpuem7HoBuHRxcUTGRNuVLL5sgaz3fm2SqZrCQfRkPGHGHSuhZDb2ONVNo6nC@z8cck48g4ws0k0ccgkc40kok:27017/?directConnection=true"
 
 const connectToMongo = async () => {
   try {
     mongoose.set("strictQuery", false);
-    mongoose.connect("mongodb://root:ZWDYpuem7HoBuHRxcUTGRNuVLL5sgaz3fm2SqZrCQfRkPGHGHSuhZDb2ONVNo6nC@z8cck48g4ws0k0ccgkc40kok:27017/?directConnection=true");
+    mongoose.connect(process.env.mongoURI);
     console.log("Connected to Mongo Successfully!");
   } catch (error) {
     console.log(error);
